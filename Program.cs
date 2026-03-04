de5f0c8c3691d34970d65c5e8d0bade5d8a0f63d
ahmetsenyuz <89253941+ahmetsenyuz@users.noreply.github.com>
2026-03-04 21:11:22 +0300

    BI-0003: Register TodoService in Program.cs for dependency injection

--- Program.cs
+++ Program.cs
@@ -3,6 +3,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+// Register the TodoService
+builder.Services.AddSingleton<ITodoService, TodoService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
